fe4698c7feba2f133a397f76a322ae7a9dd0b99b
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-22 08:33:15 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan42/tabungan/PBO210118068Latihan42Tabungan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan42.tabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk menghitung tabungan
+ */
+public class PBO210118068Latihan42Tabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====Program Penarikan Uang====");
+        System.out.print("Masukkan Saldo Awal : ");
+        Tabungan tabungan = new Tabungan(scanner.nextInt());
+        System.out.print("Jumlah uang yang diambil : ");
+        System.out.println("Saldo Anda Sekarang : "
+                + tabungan.ambilUang(scanner.nextInt()));
+    }
+    
+}
---
+++ src/pbo2/pkg10118068/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan42.tabungan;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk menghitung tabungan
+ */
+public class Tabungan {
+    private int saldo;
+    
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+}
